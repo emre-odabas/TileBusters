f60c5d05cdb53384c0a5a79da8611a380e03afd0
Emre Odabaş <emreodabas_32@hotmail.com>
2024-05-04 19:16:47 +0300

    remove unused assets

--- Assets/puzzle/Resources/Scripts/Puzzle/Data/ScriptableObjects/TripleTileGameDesginEditorMediatorSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class TripleTileGameDesginEditorMediatorSO : ScriptableObject
-{
-    public bool PlayDemo = false;
-    public string DemoScenePath = "Assets/Scenes/TripleTileDemo.unity";
-    public string DemoDataPath = "Assets/Editor/PlayDemoStageData.asset";
-    public string DataOutputPath =  "Assets/StageData/{0}.asset";
-}
